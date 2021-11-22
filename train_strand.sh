311fd94d8985243e1ff7c29fcd0fd47539f5a530
shivani0109 <shivanigauniyal@gmail.com>
2021-11-22 13:10:25 +0100

    CHanged directory and environment

--- train_strand.sh
+++ train_strand.sh
@@ -8,7 +8,7 @@
 #SBATCH --account=sharmas
 #SBATCH --partition=pGPU32
 #SBATCH --exclusive
-#SBATCH --output=/gpfs/home/sharmas/mc-snow-autoconversion/Notebooks/output_logs/slurm-%j.out
+#SBATCH --output=/gpfs/work/sharmas/mc-snow-data/output_logs/slurm-%j.out
 
 
-srun /gpfs/home/sharmas/miniconda3/bin/python3 train_save.py
+srun /gpfs/home/sharmas/miniconda3/envs/warm-rain/bin/python3 train_save.py $1
