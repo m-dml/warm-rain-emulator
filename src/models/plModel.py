7749cc371a63433716648a813cef115591475730
shivani0109 <shivanigauniyal@gmail.com>
2021-11-22 13:11:32 +0100

    Added option for calculating absolute loss

--- src/models/plModel.py
+++ src/models/plModel.py
@@ -15,17 +15,24 @@ from src.models.nnmodel import plNetwork
 
 class LightningModel(pl.LightningModule):    
 
-    def __init__(self,updates_mean,updates_std,batch_size=256,beta=0.35,learning_rate=2e-4,act=nn.ReLU(),n_layers=5,ns=200,out_features=4,depth=9,p=0.25):
+    def __init__(self,updates_mean,updates_std,batch_size=256,beta=0.35,
+                 learning_rate=2e-4,act=nn.ReLU(),n_layers=5,ns=200,out_features=4,
+                 depth=9,p=0.25,norm="Standard_Norm",inputs_mean=None,inputs_std=None,ic="All",
+                 use_regularizer=False,multi_step=False,step_size=None,loss_absolute=False):
         super().__init__()
         self.lr=learning_rate
         self.beta=beta
         self.batch_size=batch_size
-        self.save_hyperparameters()
-        
+        self.norm=norm
         self.updates_std=updates_std
         self.updates_mean=updates_mean
+        self.ic=ic
+        self.inputs_mean=inputs_mean
+        self.inputs_std=inputs_std
+        self.use_regularizer=use_regularizer
         self.model=self.initialization_model(act,n_layers,ns,out_features,depth,p)
-      
+        self.loss_absolute=loss_absolute
+        self.save_hyperparameters()      
     
 
     @staticmethod
@@ -41,12 +48,40 @@ class LightningModel(pl.LightningModule):
         return predictions
        
       
-    def loss_function(self,pred,updates):
+    def loss_function(self,pred,updates,x,y):
         criterion=torch.nn.SmoothL1Loss(reduction='mean', beta=self.beta)
-       
-        Lc=(pred[:,0]*self.updates_std[0])+self.updates_mean[0]
-        Lr=(pred[:,2]*self.updates_std[2])+self.updates_mean[2]
-        loss= criterion(updates,pred) + criterion(Lc,(-Lr))
+        if self.norm=="Rel":
+            Lc_in=((x[:,0]*self.inputs_std[0])+self.inputs_mean[0])
+            Lr_in=((x[:,2]*self.inputs_std[2])+self.inputs_mean[2])
+            
+            #Lc=pred[:,0] * Lc_in
+            #Lr=pred[:,2] * Lr_in
+            #Lc=pred[:,0]*x[:,2]
+            #Lc=(Lc*self.updates_std[0])+self.updates_mean[0]
+            #Lr=(Lr*self.updates_std[2])+self.updates_mean[2]
+            loss= criterion(updates,pred)#+ criterion(Lc,(-Lr))
+            print ("Loss: {}".format(loss)) 
+
+        else:
+            Lc=(pred[:,0]*self.updates_std[0])+self.updates_mean[0]
+            Lr=(pred[:,2]*self.updates_std[2])+self.updates_mean[2]
+            mass_cons= criterion(Lc,(-Lr))
+            if self.loss_absolute==True:
+                real_pred=(pred[:,:]*self.updates_std.reshape(1,-1)) + self.updates_mean.reshape(1,-1)
+                real_x=(x[:,:]*self.inputs_std.reshape(1,-1)) + self.inputs_mean.reshape(1,-1)
+                pred_moment=real_x + real_pred*20
+                pred_loss= criterion(pred_moment,y)
+                 
+            elif self.loss_absolute==False:
+                pred_loss= criterion(updates,pred)
+                
+            L1_reg = torch.tensor(0., requires_grad=True)
+            if self.use_regularizer==True:
+                for name, param in self.model.named_parameters():
+                    if 'weight' in name:
+                        L1_reg = L1_reg + torch.norm(param, 1)
+                        
+            loss= pred_loss + mass_cons + 10e-4 * L1_reg
         
         #loss= criterion(updates,preds)
         return loss
@@ -62,7 +97,7 @@ class LightningModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x,updates,rates,y = batch
         logits = self.forward(x)
-        loss = self.loss_function(logits, updates)
+        loss = self.loss_function(logits, updates,x,y)
         # Add logging
         self.log("train_loss", loss)
   
@@ -73,7 +108,7 @@ class LightningModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x,updates,rates, y = batch
         logits = self.forward(x)
-        loss = self.loss_function(logits, updates)
+        loss = self.loss_function(logits, updates,x,y)
        
         self.log("val_loss", loss)
         return loss
@@ -84,7 +119,7 @@ class LightningModel(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         x,updates,rates, y = batch
         logits = self.forward(x)
-        loss = self.loss_function(logits, updates)
+        loss = self.loss_function(logits, updates,x,y)
         
         predictions_pred.append(logits)
         predictions_actual.append(y)
