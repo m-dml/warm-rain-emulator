af524f78781faea881ff3b730b7f8297f16a803c
shivani0109 <shivanigauniyal@gmail.com>
2021-12-17 15:16:59 +0100

    Added hard constraints on moments, mass cons in moments

--- src/models/plModel.py
+++ src/models/plModel.py
@@ -16,7 +16,9 @@ class LightningModel(pl.LightningModule):
     def __init__(self, updates_mean,updates_std,data_dir= "/gpfs/work/sharmas/mc-snow-data/",batch_size=256,beta=0.35,
                  learning_rate=2e-4,act=nn.ReLU(),loss_func=None, n_layers=5,ns=200,
                  depth=9,p=0.25,inputs_mean=None,inputs_std=None,
-                 mass_cons_loss=False,loss_absolute=False,multi_step=False,step_size=1,moment_scheme=2):
+                 mass_cons_loss_updates=False,loss_absolute=False,mass_cons_loss_moments=False,hard_constraints_updates=False,
+                 hard_constraints_moments=False,
+                 multi_step=False,step_size=1,moment_scheme=2):
         super().__init__()
         #self.automatic_optimization = False
         self.moment_scheme= moment_scheme
@@ -25,11 +27,22 @@ class LightningModel(pl.LightningModule):
         self.loss_func = loss_func
         self.beta=beta
         self.batch_size=batch_size
-        
-        
-        
+
         self.loss_absolute=loss_absolute
-        self.mass_cons_loss = mass_cons_loss
+        self.mass_cons_loss_updates = mass_cons_loss_updates
+        self.mass_cons_loss_moments = mass_cons_loss_moments
+
+        #Using the following only makes sense for multi-step training
+        self.hard_constraints_updates = hard_constraints_updates
+        self.hard_constraints_moments = hard_constraints_moments 
+
+        if self.hard_constraints_moments == True and self.hard_constraints_updates == False:
+            print ("Setting hard constrainsts on moments while none on updates can lead to problems")
+
+        if self.hard_constraints_moments == False and self.mass_cons_loss_moments == True:
+            print("not using hard constraints on updates while choosing to conserve mass can lead to negative moments")
+
+        
         self.multi_step=multi_step
         self.step_size=step_size
         self.num_workers=48
@@ -61,17 +74,17 @@ class LightningModel(pl.LightningModule):
         self.predictions = predictions.float()
        
         #un-normalize logits
-        if self.mass_cons_loss == False:
+        if self.hard_constraints_updates == True:
             
             
-            self.real_pred = self.predictions * self.updates_std + self.updates_mean
+            pred_moment = self.predictions * self.updates_std + self.updates_mean
             
             """Checking the predicted values"""
-            self.real_pred[:,0] = torch.min(self.real_pred[:,0],torch.tensor([0.]).to(self.device))
-            self.real_pred[:,1] = torch.min(self.real_pred[:,1] ,torch.tensor([0.]).to(self.device))
-            self.real_pred[:,2] = torch.max(self.real_pred[:,2] ,torch.tensor([0.]).to(self.device))
+            pred_moment[:,0] = torch.min(pred_moment[:,0],torch.tensor([0.]).to(self.device))
+            pred_moment[:,1] = torch.min(pred_moment[:,1] ,torch.tensor([0.]).to(self.device))
+            pred_moment[:,2] = torch.max(pred_moment[:,2] ,torch.tensor([0.]).to(self.device))
 
-            predictions = (self.real_pred - self.updates_mean) / self.updates_std
+            predictions = (pred_moment - self.updates_mean) / self.updates_std
           
         return predictions
     
@@ -85,15 +98,14 @@ class LightningModel(pl.LightningModule):
         
         
         #For mass conservation
-        if self.mass_cons_loss == True: 
+        if self.mass_cons_loss_updates == True: 
             
-            Lc=(pred[:,0]*self.updates_std[0])+self.updates_mean[0]
-            Lr=(pred[:,2]*self.updates_std[2])+self.updates_mean[2]
-            mass_cons= criterion(Lc,(-Lr))
+            pred[:,2] = pred[:,0]
+            #Lc=(pred[:,0]*self.updates_std[0])+self.updates_mean[0]
+            
+            #Lr=(pred[:,2]*self.updates_std[2])+self.updates_mean[2]
+            #mass_cons= criterion(Lc,(-Lr))
           
-            
-        else:
-            mass_cons = 0
             
 
         #For moments
@@ -101,22 +113,36 @@ class LightningModel(pl.LightningModule):
          # Removing Norm
 
             real_pred = pred* self.updates_std + self.updates_mean
-            real_x = x[:,:self.out_features] * self.inputs_std + self.inputs_mean
-            real_y = y * self.inputs_std + self.inputs_mean
-                
+            real_x = x[:,:self.out_features] * self.inputs_std[:self.out_features] + self.inputs_mean[:self.out_features]
+            real_y = y * self.inputs_std[:self.out_features] + self.inputs_mean[:self.out_features]
+            Lo = x[:,-2] *self.inputs_std[-2] +self.inputs_mean[-2]    
             pred_moment = real_x + real_pred*20
+
+            if self.hard_constraints_moments:
+                #Check moments
+                pred_moment [:,0] = torch.max(pred_moment[:,0],torch.tensor([0.]).to(self.device))
+                pred_moment[:,1] = torch.max(pred_moment[:,1] ,torch.tensor([0.]).to(self.device))
+                pred_moment[:,2] = torch.max(pred_moment[:,2] ,torch.tensor([0.]).to(self.device))
+                pred_moment[:,3] = torch.max(pred_moment[:,3] ,torch.tensor([0.]).to(self.device))
+
+            if self.mass_cons_loss_moments:
+                #Best not to use if hard constrainsts are not used in moments
+                pred_moment[:,2] = Lo - pred_moment[:,0]
+                
             pred_moment_norm = torch.empty((y.shape), dtype=torch.float32,device = self.device)
-            pred_moment_norm[:,:self.out_features] = (pred_moment - self.inputs_mean[self.out_features]) / self.inputs_std[self.out_features]
+            pred_moment_norm[:,:self.out_features] = (pred_moment - self.inputs_mean[:self.out_features]) / self.inputs_std[:self.out_features]
                
 
             pred_loss= criterion(pred_moment_norm,y) 
-                 
+
+            return pred_loss, pred_moment #Returning here so it doesn't need to be calculated again in calc_new_x
+        
         elif self.loss_absolute==False:
             pred_loss= criterion(updates,pred)
             
-        loss_net= pred_loss + mass_cons 
+            return pred_loss 
         
-        return loss_net
+        
         
         
 
@@ -132,15 +158,16 @@ class LightningModel(pl.LightningModule):
            Dim updates, y: batch, moments,step size"""
            
         x,updates,y = batch
+        
         for k in range (self.step_size):
             
             pred = self.forward(x.float())
             with torch.no_grad():
-                self.loss [k,:] = self.loss_function(pred.float(), updates[:,:,k].float(),x.float(),y[:,:,k].float())
+                self.loss [k,:],pred_moment = self.loss_function(pred.float(), updates[:,:,k].float(),x.float(),y[:,:,k].float())
             
             
             if self.step_size > 1:
-                x=(self.calc_new_x(x,pred,y[:,:,k].float(),k)).float()
+                x=(self.calc_new_x(x,pred,y[:,:,k].float(),k),pred_moment).float()
             new_str="Train_loss_" + str(k)
             self.log(new_str, self.loss[k])
        
@@ -161,10 +188,10 @@ class LightningModel(pl.LightningModule):
         for k in range (self.step_size):
             pred = self.forward(x.float())
             with torch.no_grad():
-                self.loss[k,:] = self.loss_function(pred.float(), updates[:,:,k].float(),x.float(),y[:,:,k].float())
+                self.loss[k,:],pred_moment = self.loss_function(pred.float(), updates[:,:,k].float(),x.float(),y[:,:,k].float())
           
             if self.step_size > 1:
-                x=(self.calc_new_x(x,pred,y[:,:,k].float(),k)).float()
+                x=(self.calc_new_x(x,pred,y[:,:,k].float(),k),pred_moment).float()
 
         
         
@@ -185,14 +212,9 @@ class LightningModel(pl.LightningModule):
         self.predictions_actual.append(y)
         return pred,y
     
-    def calc_new_x(self,x,pred,y,k): 
+    def calc_new_x(self,x,pred,y,k,pred_moment): 
+        
         #un-normalize logits
-           
-        real_pred = pred * self.updates_std + self.updates_mean
-        real_x = x[:,:self.out_features] * self.inputs_std[:self.out_features] + self.inputs_mean[:self.out_features]
-           
-        #calc new x
-        pred_moment = real_x + real_pred*20
             
         pred_moment_norm=torch.empty((x.shape), dtype=torch.float32, device = self.device)
         pred_moment_norm[:,:self.out_features] = (pred_moment - self.inputs_mean[:self.out_features]) / self.inputs_std[:self.out_features]
@@ -206,9 +228,12 @@ class LightningModel(pl.LightningModule):
         pred_moment_norm[:,6:]=x[:,6:]  #Doesn't need to be normalized/un-normalized
         
          #Add plotting here
-        if (self.global_step % 2000) == 0:
-            fig,figname = self.plot_preds(x=real_x[:,:self.out_features],x_pred = pred_moment)
-            self.logger.experiment.add_figure(figname + ": Step  " +str(k), fig, self.global_step)
+        if self.plot_while_training:
+            real_pred = pred * self.updates_std + self.updates_mean
+            real_x = x[:,:self.out_features] * self.inputs_std[:self.out_features] + self.inputs_mean[:self.out_features]
+            if (self.global_step % 2000) == 0:
+                fig,figname = self.plot_preds(x=real_x[:,:self.out_features],x_pred = pred_moment)
+                self.logger.experiment.add_figure(figname + ": Step  " +str(k), fig, self.global_step)
         
         return pred_moment_norm
     
