71644ce1842926bce9cac7893f2d2faf87cdfd6f
shivani0109 <shivanigauniyal@gmail.com>
2022-01-10 15:40:16 +0100

    Removing unnecessary imports

--- src/models/plModel.py
+++ src/models/plModel.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from src.models.nnmodel_exp import plNetwork
+from src.models.nnmodel import plNetwork
 from matplotlib import pyplot as plt
 import seaborn as sns
 
