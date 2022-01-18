541a333d9bf89dac2e406e040614806acbb1d90e
shivani0109 <shivanigauniyal@gmail.com>
2022-01-18 18:23:43 +0100

    Made dropout optional

--- src/models/nnmodel.py
+++ src/models/nnmodel.py
@@ -1,39 +1,5 @@
-import numpy as np
-
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-
-class Network(nn.Module):
-    def __init__(self, act, n_layers=5, ns=200, out_features=4, depth=9, p=0.25):
-
-        super().__init__()
-
-        self.layers = []
-
-        in_features = depth
-        for i in range(n_layers):
-
-            self.layers.append(nn.Linear(in_features, ns))
-            # nn.init.kaiming_normal_(self.layers[i].weight, mode='fan_out')
-            in_features = ns
-
-        self.layers.append(nn.Linear(ns, out_features))
-        self.layers = nn.ModuleList(self.layers)
-        self.dropout = nn.Dropout(p)
-        self.activation = act
-        self.n_layers = n_layers
-
-    def forward(self, x):
-
-        for i in range(self.n_layers - 1):
-            x = self.activation(self.layers[i](x))
-            x = self.dropout(x)
-
-        return x
 
 
 class plNetwork(nn.Module):
@@ -46,6 +12,7 @@ class plNetwork(nn.Module):
         depth=9,
         p=0.25,
         use_batch_norm=False,
+        use_dropout=False
     ):
 
         super().__init__()
@@ -67,15 +34,16 @@ class plNetwork(nn.Module):
         self.dropout = nn.Dropout(p)
         self.activation = act
         self.n_layers = n_layers
+        self.use_dropout = use_dropout
 
     def _forward_impl(self, x):
         for i in range(self.n_layers - 1):
             x = self.activation(self.layers[i](x))
-            x = self.dropout(x)
+            if self.use_dropout:
+                x = self.dropout(x)
 
         x = self.layers[-1](x)
 
-        x = self.layers[-1](x)
         if x.shape[-1] == 3:
             delLc = -x[:, 1]
             x = torch.cat((delLc.reshape(-1, 1), x), axis=1)
