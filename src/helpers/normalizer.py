Changed normalization of residuals, Fixes #17

src/helpers/normalizer.py
@@ -9,6 +9,7 @@ class normalizer:
         self,
         updates,
         inputs,
+        outputs,
         updates_mean,
         updates_std,
         inputs_mean,
@@ -22,19 +23,24 @@ class normalizer:
 
         self.updates = updates
         self.x = inputs
+        self.y = y
         self.updates_mean = updates_mean
         self.updates_std = updates_std
         self.inputs_mean = inputs_mean
         self.inputs_std = inputs_std
         self.out_features = out_features
-        self.real_x = self.real_updates = self.pred_moment = self.pred_moment_norm = None
+        self.real_x = self.real_y = (
+            self.real_updates
+        ) = self.pred_moment = self.pred_moment_norm = None
         self.hard_constraints_updates = hard_constraints_updates
         self.hard_constraints_moments = hard_constraints_moments
         self.mass_cons_moments = mass_cons_moments
         self.device = device
 
     def calc_preds(self):
-        self.real_updates = self.updates * self.updates_std + self.updates_mean  # Un-normalize
+        self.real_updates = (
+            self.updates * self.updates_std + self.updates_mean
+        )  # Un-normalize
 
         self.real_x = (
             self.x[:, : self.out_features] * self.inputs_std[: self.out_features]
@@ -43,20 +49,29 @@ class normalizer:
 
         self.pred_moment = self.real_x + self.real_updates * 20
 
-        self.pred_moment_norm = (self.pred_moment - self.inputs_mean[: self.out_features]) / self.inputs_std[
-            : self.out_features
-        ]
-
-        return self.real_x, self.pred_moment, self.pred_moment_norm
+        self.pred_moment_norm = (
+            self.pred_moment - self.inputs_mean[: self.out_features]
+        ) / self.inputs_std[: self.out_features]
+        self.real_y = (
+            self.y[:, : self.out_features] * self.inputs_std[: self.out_features]
+            + self.inputs_mean[: self.out_features]
+        )
+        return self.real_x, self.realy_y, self.pred_moment, self.pred_moment_norm
 
     def set_constraints(self):
-
+        assert self.updates is not None
         if self.hard_constraints_updates:
 
             """Checking the predicted values"""
-            del_lc = torch.min(self.real_updates[:, 0], torch.tensor([0.0]).to(self.device)).reshape(-1, 1)
-            del_nc = torch.min(self.real_updates[:, 1], torch.tensor([0.0]).to(self.device)).reshape(-1, 1)
-            del_lr = torch.max(self.real_updates[:, 2], torch.tensor([0.0]).to(self.device)).reshape(-1, 1)
+            del_lc = torch.min(
+                self.real_updates[:, 0], torch.tensor([0.0]).to(self.device)
+            ).reshape(-1, 1)
+            del_nc = torch.min(
+                self.real_updates[:, 1], torch.tensor([0.0]).to(self.device)
+            ).reshape(-1, 1)
+            del_lr = torch.max(
+                self.real_updates[:, 2], torch.tensor([0.0]).to(self.device)
+            ).reshape(-1, 1)
             del_nr = self.real_updates[:, 3].reshape(-1, 1)
             self.real_updates = torch.cat((del_lc, del_nc, del_lr, del_nr), axis=1)
             self.updates = (
@@ -67,7 +82,9 @@ class normalizer:
             + self.inputs_mean[: self.out_features]
         )  # un-normalize
         self.pred_moment = self.real_x + self.real_updates * 20
-        Lo = self.x[:, -3] * self.inputs_std[-3] + self.inputs_mean[-3]  # For total water content
+        Lo = (
+            self.x[:, -3] * self.inputs_std[-3] + self.inputs_mean[-3]
+        )  # For total water content
 
         if self.hard_constraints_moments:
 
@@ -86,9 +103,13 @@ class normalizer:
         if self.mass_cons_moments:
 
             """Best not to use if hard constraints are not used in moments"""
-            self.pred_moment[:, 0] = Lo - self.pred_moment[:, 2]  # Lc calculated from Lr
+            self.pred_moment[:, 0] = (
+                Lo - self.pred_moment[:, 2]
+            )  # Lc calculated from Lr
 
-        self.pred_moment_norm = (self.pred_moment - self.inputs_mean[: self.out_features]) / self.inputs_std[
+        self.pred_moment_norm = (
+            self.pred_moment - self.inputs_mean[: self.out_features]
+        ) / self.inputs_std[
             : self.out_features
         ]  # Normalized value of predicted moments (not updates)
 

src/models/plModel.py
@@ -105,6 +105,7 @@ class LightningModel(pl.LightningModule):
         )
         (
             self.real_x,
+            self.real_y,
             self.pred_moment,
             self.pred_moment_norm,
         ) = self.norm_obj.calc_preds()
@@ -124,8 +125,8 @@ class LightningModel(pl.LightningModule):
 
             pred_loss = self.criterion(self.pred_moment_norm, y)
             try:
-                assert (self.training is True) and (self.global_step % 10 == 0)
-                assert k is not None and self.plot_all_moments is True
+                assert (self.training is True) and self.plot_all_moments is True
+                #assert k is not None and (self.global_step % 10 == 0)
                 self._plot_all_moments(y, k)
             except:
                 pass
@@ -181,7 +182,7 @@ class LightningModel(pl.LightningModule):
             self.forward()
             assert self.updates is not None
             self.loss_each_step = self.loss_function(updates[:, :, k], y[:, :, k], k)
-            val_preds_step.append(self.pred_moment_norm.cpu().numpy())
+            val_preds_step.append(self.pred_moment.cpu().numpy())
             self.cumulative_loss = self.cumulative_loss + self.loss_each_step
             if self.step_size > 1:
                 self.calc_new_x(y[:, :, k], k)
@@ -191,7 +192,7 @@ class LightningModel(pl.LightningModule):
         val_preds_step = np.asarray(val_preds_step)
         val_preds_step = np.moveaxis(val_preds_step, 0, -1)
 
-        return {"preds": val_preds_step, "y": y.cpu().numpy()}
+        return {"preds": val_preds_step, "y": self.real_y.cpu().numpy()}
 
     def validation_epoch_end(self, validation_step_outputs):
         # validation_step_outputs is a list of dictionaries